d5a875ad001319d97c0ef3baa692deb54b0d65bb
nyagami <hoangquan05112002@gmail.com>
2023-01-11 21:25:21 +0700

    fix lint

--- judge/views/single_svg.py
+++ judge/views/single_svg.py
@@ -7,4 +7,4 @@ def view(request, tex, hash):
     root = settings.TEXOID_CACHE_ROOT if tex == 'texoid' else settings.MATHOID_CACHE_ROOT
     svg_path = os.path.join(root, hash, 'svg')
     svg_data = open(svg_path, 'r').read()
-    return HttpResponse(svg_data, content_type="image/svg+xml")
+    return HttpResponse(svg_data, content_type='image/svg+xml')
